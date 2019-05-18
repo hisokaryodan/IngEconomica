42a43ebdc3fd56014f9957c4c49746e79bfee7c4
CAMILO <camilo@debian>
2019-05-18 14:17:29 -0500

    creacion de flujo de funciones

--- economia.js
+++ economia.js
@@ -1,15 +1,68 @@
-function calcularCuota(vP, n, i) {
+function getCalcularCuota(vP, n, i) {
     return vP / ((1 - (Math.pow((1 + i), -n))) / i)
 }
 
-function calcularValorPresente(cuota, i, n) {
+function getCalcularValorPresente(cuota, i, n) {
     return cuota * ((1 - (Math.pow((1 + i), -n))) / i)
 }
 
-var vP = 28000000;
-var n = 72;
-var i = 0.015;
-var cuota = calcularCuota(vP, n, i);
-var N = 61
+function getTotalAbono(abono, i, newVp) {
+    var interesADescontar = newVp * i;
 
-console.log(calcularValorPresente(cuota, i, N))
+    return abono - interesADescontar;
+}
+
+function getNuevoSaldo(newVp, totalAbono) {
+    return newVp - totalAbono
+}
+
+function getDisminucionTiempo(nuevoSaldo, cuota, i) {
+    var division = ((nuevoSaldo / cuota) * i) - 1;
+    console.log('nuevo valor -->', nuevoSaldo)
+    var potencia = -(1 + i);
+    var logaritmoIzquierdo = Math.log((-1 * division));
+    var logaritomoDerecho = Math.log((-1 * potencia));
+
+    return (logaritmoIzquierdo / logaritomoDerecho) - mes
+}
+
+function getMesePorPagar(n, mes, cuota = true) {
+    return cuota === false ? n - (mes - 1) : n - mes;
+}
+
+/* flujo tiempo*/
+// var i = 0.015;// valor obtenido codigo brayan
+// var vP = 28000000; // valor entrada
+// var n = 72; // valor de entrada
+// var mes = 12; // valor de entrada
+// var abono = 4000000; // valor de entrada
+// var cuota = getCalcularCuota(vP, n, i);
+// var N = getMesePorPagar(n, mes);
+// var newVp = getCalcularValorPresente(cuota, i, N);
+// console.log('newVp--->', newVp)
+// var totalAbono = getTotalAbono(abono, i, newVp);
+// var nuevoSaldo = getNuevoSaldo(newVp, totalAbono);
+
+
+/*flujo disminucion de cuota*/
+var i = 0.015;// valor obtenido codigo brayan
+var vP = 21785333; // valor entrada
+var n = 60.14; // valor de entrada
+var mes = 24; // valor de entrada
+var abono = 3000000; // valor de entrada
+var cuota = 638618.15 // tiene estar guardado en un array
+var N = getMesePorPagar(n, mes, false);
+console.log('esta monda es N--->', N)
+var newVp = getCalcularValorPresente(cuota, i, N);
+console.log('newVp--->', newVp)
+var totalAbono = getTotalAbono(abono, i, newVp);
+var nuevoSaldo = getNuevoSaldo(newVp, totalAbono);
+
+// console.log(calcularValorPresente(cuota, i, N));
+// console.log(calcularCuota(vP, n, i));
+// console.log(getTotalAbono(abono, i, newVp));
+// console.log(nuevoSaldo(newVp, totalAbono));
+
+var N = getMesePorPagar(n, mes, true);
+// console.log(getDisminucionTiempo(nuevoSaldo, cuota, i))
+console.log(getCalcularCuota(nuevoSaldo, N, i));
